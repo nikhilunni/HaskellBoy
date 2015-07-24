a29a131efc59682da7c3c1fe03ac1b517e435b75
Nikhil Unni <unni@adobe.com>
2015-07-23 18:27:43 -0700

    started emulator typeclass

--- src/Register.hs
+++ src/Register.hs
@@ -1,66 +1,57 @@
 module Register 
-       ( Register
-       , Registers
-       , makeRegisters
-       )
-where
+         ( Register
+         )
+ where
        
-import Control.Applicative
-import Control.Monad.ST
-import Data.Word
-import Data.STRef
+ import Control.Applicative
+ import Control.Monad.ST
+ import Data.Word
+ import Data.STRef
+ import Data.Monoid
 
-type Register s = STRef s Word32
+ type Mem8 s  = STRef s Word8
+ type Mem16 s = STRef s Word16 
+ 
 
--- See : http://problemkaputt.de/gbatek.htm#armcpuregisterset
-data Registers s = Registers {
-      r0 :: (Register s)
-    , r1 :: (Register s)
-    , r2 :: (Register s)
-    , r3 :: (Register s)
-    , r4 :: (Register s)
-    , r5 :: (Register s)
-    , r6 :: (Register s)
-    , r7 :: (Register s)
-    , r8 :: (Register s)
-    , r8_fiq :: (Register s)
-    , r9 :: (Register s)
-    , r9_fiq :: (Register s)
-    , r10 :: (Register s)
-    , r10_fiq :: (Register s)
-    , r11 :: (Register s)
-    , r11_fiq :: (Register s)
-    , r12 :: (Register s)
-    , r12_fiq :: (Register s)
-    , r13 :: (Register s)
-    , r13_fiq :: (Register s)
-    , r13_svc :: (Register s)
-    , r13_abt :: (Register s)
-    , r13_irq :: (Register s)
-    , r13_und :: (Register s)
-    , r14 :: (Register s)
-    , r14_fiq :: (Register s)
-    , r14_svc :: (Register s)
-    , r14_abt :: (Register s)
-    , r14_irq :: (Register s)
-    , r14_und :: (Register s)
-    , r15 :: (Register s)
-    , cspr :: (Register s)
-    , spsr_fiq :: (Register s)
-    , spsr_svc :: (Register s)
-    , spsr_abt :: (Register s)
-    , spsr_irq :: (Register s)
-    , spsr_und :: (Register s)
-}
+ data Register s = Register { a  :: {-# UNPACK #-} !(Mem8  s)
+                            , f  :: {-# UNPACK #-} !(Mem8  s)
+                            , b  :: {-# UNPACK #-} !(Mem8  s)
+                            , c  :: {-# UNPACK #-} !(Mem8  s)
+                            , d  :: {-# UNPACK #-} !(Mem8  s)
+                            , e  :: {-# UNPACK #-} !(Mem8  s)
+                            , h  :: {-# UNPACK #-} !(Mem8  s)
+                            , l  :: {-# UNPACK #-} !(Mem8  s)
+                            , af :: {-# UNPACK #-} !(Mem16 s)
+                            , bc :: {-# UNPACK #-} !(Mem16 s)
+                            , de :: {-# UNPACK #-} !(Mem16 s)
+                            , hl :: {-# UNPACK #-} !(Mem16 s)
+                            , sp :: {-# UNPACK #-} !(Mem16 s)
+                            , pc :: {-# UNPACK #-} !(Mem16 s)
+                            }
 
-makeRegisters :: ST s (Registers s)
-makeRegisters = let k = newSTRef 0 in Registers
-    <$> k <*> k <*> k <*> k <*> k -- 5
-    <*> k <*> k <*> k <*> k <*> k -- 10
-    <*> k <*> k <*> k <*> k <*> k -- 15
-    <*> k <*> k <*> k <*> k <*> k -- 20
-    <*> k <*> k <*> k <*> k <*> k -- 25
-    <*> k <*> k <*> k <*> k <*> k -- 30
-    <*> k <*> k <*> k <*> k <*> k -- 35
-    <*> k <*> k                   -- 37 total registers
+
+ data Register =   A
+                 | F
+                 | B
+                 | C
+                 | D
+                 | E
+                 | H
+                 | L
+                 | AF
+                 | BC
+                 | DE
+                 | HL
+                 | SP
+                 | PC
+
+ 
+
+ initRegisters :: ST s (Register s)
+ initRegisters = let k8 = newSTRef  (0 :: Word8)
+                     k16 = newSTRef (0 :: Word16)
+                 in Register
+    <$> k8  <*> k8  <*> k8  <*> k8  <*> k8
+    <*> k8  <*> k8  <*> k8  <*> k16 <*> k16
+    <*> k16 <*> k16 <*> k16 <*> k16
 
